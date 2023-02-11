78d48842d1453d4b9aa311685077c2e0dd54f414
Tadas Paulauskas <paulauskastds@gmail.com>
2023-02-11 15:57:29 +0200

    Initialize

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:advicer_smart_api/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'application/core/services/theme_service.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeService(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeService>(
+      builder: (context, themeService, child) {
+        return MaterialApp(
+          themeMode:
+              themeService.isDarkModeOn ? ThemeMode.dark : ThemeMode.light,
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          home: const Placeholder(),
+        );
+      },
+    );
+  }
+}
